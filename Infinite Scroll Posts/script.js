bb31d0d361f2ba8deb380966121ac567b2749f88
kiseo <5rne@naver.com>
2021-12-03 23:56:31 +0900

    infinite scroll post

--- Infinite Scroll Posts/script.js
+++ Infinite Scroll Posts/script.js
@@ -9,7 +9,6 @@
  async function getPosts(){
     const res = await fetch(`http://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`)
     const data = await res.json();
- 
     return data;
  }
 
